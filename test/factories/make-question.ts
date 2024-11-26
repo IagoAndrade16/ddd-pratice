88c40fd5696f2e8f20490ed7db30cc7612f39c7e
ïago alexandre <ago.alexandre@paymobi.com.br>
2024-11-26 05:56:24 -0300

    add faker library to run tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override?: Partial<QuestionProps>): Question {
-  const question = Question.create({
-    authorId: new UniqueEntityID('authorId'),
-    content: 'content',
-    title: 'title question',
-    slug: Slug.create('example-question'),
-    ...override,
-  })
+export function makeQuestion(
+  override?: Partial<QuestionProps>,
+  id?: UniqueEntityID,
+): Question {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID('authorId'),
+      content: faker.lorem.text(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
