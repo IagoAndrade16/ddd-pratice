1fef3348730ceef58f02370cce64d9f5d205cb91
ïago alexandre <iagoaap16@gmail.com>
2024-12-04 06:01:19 -0300

    domain events classes

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,4 +1,21 @@
+import { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domain-events'
 import { Entity } from './entity'
 
 // Entidade principal que representa um objeto de domínio que é agregado de outros objetos de domínio.
-export abstract class AggregateRoot<Props> extends Entity<Props> {}
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domainEvents.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents(): void {
+    this._domainEvents = []
+  }
+}
